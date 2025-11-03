b94cbf8b3c0489ff5f27f7d52c5f5d5920c9a749
ltanak <ltanakbusiness@outlook.com>
2025-11-03 21:55:14 +0000

    generator collection working

---
+++ src/order_simulation/order_simulation.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <optional>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <iostream>
+
+class OrderSimulation {
+    public:
+        OrderSimulation(){}
+};
