1c9331331cd092fc41f833cd6f4ed8e850e510b7
ltanakwebsite <ltanakbusiness@outlook.com>
2025-11-07 14:32:38 +0000

    test name

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,6 @@ int main() {
      * Create "framework" / section where we will put the endpoint of the lock-free data structure to start getting ready for that
      */
 
-     
     CollectionOrderGenerator<Order> collection(gens, 42);
 
     Order o = orderGen.generate();
