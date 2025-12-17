5a29259c97ba0ea58c4eeec23bf10de8095a0b9f
ltanak <ltanakbusiness@outlook.com>
2025-12-17 15:16:30 +0000

    benchmarkign functions

--- src/benchmarking/python/latency_plots/utils.py
+++ src/benchmarking/python/latency_plots/utils.py
@@ -15,5 +15,14 @@ def csvDir(dir: str) -> Path:
 def all_csvs(path: Path, reverse: bool = False):
     return sorted(path.glob("*.csv"), reverse=reverse)
 
-def getMostRecentCsv(path: Path) -> str:
+def getMostRecentCsv(path: Path) -> Path:
     return all_csvs(path, True)[0]
+
+def latencyPlotsDir():
+    """
+    Returns:
+        benchmarking/python/latency_plots/graphs
+    """
+    path = getBenchmarkingRoot() / "python" / "latency_plots" / "graphs"
+    path.mkdir(parents=True, exist_ok=True)
+    return path
