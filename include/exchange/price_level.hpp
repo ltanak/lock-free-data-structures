2a9d8cbd66041b6ad83f865ba1d592b7c561a3c0
ltanak <ltanakbusiness@outlook.com>
2025-12-29 14:37:28 +0000

    side tracked and did python

--- include/exchange/price_level.hpp
+++ include/exchange/price_level.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <sstream>
+#include <iostream>
 #include "exchange/book_order.hpp"
 
 // double ended queue
-struct PriceLevel {
+struct alignas(64) PriceLevel {
     BookOrder* head;
     BookOrder* tail;
     int total_quantity;
 
     auto enqueue(BookOrder*) -> void;
     auto dequeue(BookOrder*) -> void;
-    bool isEmpty();
+    auto isEmpty() -> bool;
 };
