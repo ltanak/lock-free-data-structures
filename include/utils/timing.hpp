d8581c876d7dc1ceb28d33bfc61905392dd67914
Louis Tanak <louistanak@Louiss-MacBook-Pro.local>
2026-01-17 10:56:09 +0000

    mac asm

--- include/utils/timing.hpp
+++ include/utils/timing.hpp
@@ -5,12 +5,19 @@
 #include <chrono>
 
 namespace ltime {
-
+#ifdef __ARM_ARCH_ISA_A64
+    inline auto rdtsc_lfence() -> uint64_t {
+        uint64_t val;
+        asm volatile("isb; mrs %0, cntvct_el0" : "=r"(val) :: "memory");
+        return val;
+    }
+#else
     inline auto rdtsc_lfence() -> uint64_t {
         uint32_t lo, hi, aux;
         asm volatile("lfence; rdtsc" : "=a"(lo), "=d"(hi) :: "memory");
         return (hi << 32) | lo;
     }
-    
+#endif
     auto measure_tsc_ghz() -> double;
 }
+
