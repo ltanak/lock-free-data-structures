8bae81ea4aa3de6d4471752ee31964b1f5c22760
ltanak <ltanakbusiness@outlook.com>
2026-01-23 17:33:46 +0000

    Wip: CPU pinning improved performance, but cache warming still doesn't work

--- include/utils/threads.hpp
+++ include/utils/threads.hpp
@@ -3,11 +3,15 @@
 #include <thread>
 #include <atomic>
 #include <vector>
+#include <pthread.h>
+#include <sched.h>
 
 namespace lThread {
     using namespace std;
 
     void close(vector<thread> &threads);
     void close(vector<thread> &producers, vector<thread> &consumers);
+
+    void pin_thread(int cpu);
 
 }
