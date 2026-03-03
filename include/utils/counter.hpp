6e8c87120f21784f0f2b8f998cd1d92abe9e6440
ltanak <ltanakbusiness@outlook.com>
2026-03-03 11:59:11 +0000

    Wip: adding functions to benchmarkwrapper

--- include/utils/counter.hpp
+++ include/utils/counter.hpp
@@ -6,15 +6,15 @@
 
 namespace lcounter {
 #ifdef __ARM_ARCH_ISA_A64
-    inline auto rdpmc() -> uint64_t {
+    inline auto rdpmc(int counter_id) -> uint64_t {
         uint64_t val;
         asm volatile ("isb; mrs %0, pmccntr_el0" : "=r"(val));
         return val;
     }
 #else
-    inline auto rdpmc() -> uint64_t {
+    inline auto rdpmc(int counter_id) -> uint64_t {
         uint64_t lo, hi, counter;
-        asm volatile("rdpmc" : "=a"(lo), "=d"(hi) : "c"(counter));
+        asm volatile("rdpmc" : "=a"(lo), "=d"(hi) : "c"(counter_id));
         return (hi << 32) | lo;
     }
 #endif
